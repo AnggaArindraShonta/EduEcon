3972263c816ffe7318f35f2591cfe5292e28d4ea
AnggaArindraShonta <angga.shonta@students.amikom.ac.id>
2021-05-28 14:51:42 +0700

    flip pdf

--- app/src/main/java/com/example/eduecon/UI/IlmuEkonomiActivity.java
+++ app/src/main/java/com/example/eduecon/UI/IlmuEkonomiActivity.java
@@ -1,17 +1,39 @@
 package com.example.eduecon.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.example.eduecon.R;
 
 public class IlmuEkonomiActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ilmu_ekonomi);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(Uri.parse("file:///android_asset/nonbank.html").toString());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
