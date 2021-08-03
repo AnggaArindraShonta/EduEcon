e93c6e9cba706e23837bcefe667bb9c1c350b178
AnggaArindraShonta <angga.shonta@students.amikom.ac.id>
2021-08-03 15:43:48 +0700

    Terbaru

---
+++ app/src/main/java/com/eduecon/eduecon/Fragment/VideoFragment.java
@@ -0,0 +1,72 @@
+package com.eduecon.eduecon.Fragment;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.eduecon.eduecon.Model.Video;
+import com.eduecon.eduecon.R;
+import com.eduecon.eduecon.Adapter.VideoAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+
+public class VideoFragment extends Fragment {
+
+    private RecyclerView rvVideo;
+    private ArrayList<Video> videoArrayList;
+    private VideoAdapter adapterVideo;
+    private static int REQUEST_CODE=1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ActivityCompat.requestPermissions(getActivity(), new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, REQUEST_CODE);
+        View view = inflater.inflate(R.layout.fragment_video, container, false);
+
+//        setTitle("Videos");
+
+        rvVideo = view.findViewById(R.id.rvVideo);
+
+
+        loadVideoFromFirebase();
+        return view;
+    }
+
+    private void loadVideoFromFirebase() {
+        videoArrayList = new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Videos");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                videoArrayList.clear();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Video video = dataSnapshot.getValue(Video.class);
+                    videoArrayList.add(video);
+                }
+                adapterVideo = new VideoAdapter(getContext(), videoArrayList);
+                rvVideo.setAdapter(adapterVideo);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/eduecon/eduecon/UI/ui/MainActivity.java
@@ -0,0 +1,53 @@
+package com.eduecon.eduecon.UI.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.eduecon.eduecon.Fragment.CourseFragment;
+import com.eduecon.eduecon.Fragment.EbookFragment;
+import com.eduecon.eduecon.Fragment.QuizFragment;
+import com.eduecon.eduecon.Fragment.VideoFragment;
+import com.eduecon.eduecon.R;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment = null;
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        ChipNavigationBar chipNavigationBar = findViewById(R.id.chipNavigation);
+
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CourseFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(i -> {
+            switch (i) {
+                case R.id.home:
+                    fragment = new CourseFragment();
+                    break;
+                case R.id.ebook:
+                    fragment = new EbookFragment();
+                    break;
+                case R.id.assignment:
+                    fragment = new QuizFragment();
+                    break;
+                case R.id.video:
+                    fragment = new VideoFragment();
+                    break;
+            }
+
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            }
+        });
+    }
+}
